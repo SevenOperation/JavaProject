17dd2db7912fe41027cfd5e3d99c3cd781061dcd
user <user@2429-S35>
2017-04-07 10:06:36 +0200

    Created Methods for saving arrays

--- JavaProjectj/src/EingabeModule/ArraySpeichern.java
+++ JavaProjectj/src/EingabeModule/ArraySpeichern.java
@@ -1,10 +1,29 @@
 package EingabeModule;
 
+import java.io.FileOutputStream;
+
+import java.io.ObjectOutputStream;
+
+
 public class ArraySpeichern {
   public static void save(String [][] benutzerarray){
-	  
+	  try{
+		  ObjectOutputStream os = new ObjectOutputStream( new FileOutputStream("benutzer.txt"));
+          os.writeObject(benutzerarray);
+          os.close();
+	  }
+	  catch(Exception e){
+		  
+	  }
   }
  public static void save(String [][][] wohungsarray){
-	  
+	  try{
+		  ObjectOutputStream os = new ObjectOutputStream( new FileOutputStream("wohnungen.txt"));
+          os.writeObject(wohungsarray);
+          os.close();
+	  }
+	  catch(Exception e){
+		  
+	  }
   }
 }
