5c311a2d083c420b7740277766d110bdb2b09aca
user <user@2429-S35>
2017-04-07 10:00:11 +0200

    Class which handles saving arrays by serializing them and writing them

---
+++ JavaProjectj/src/EingabeModule/ArraySpeichern.java
@@ -0,0 +1,10 @@
+package EingabeModule;
+
+public class ArraySpeichern {
+  public static void save(String [][] benutzerarray){
+	  
+  }
+ public static void save(String [][][] wohungsarray){
+	  
+  }
+}
