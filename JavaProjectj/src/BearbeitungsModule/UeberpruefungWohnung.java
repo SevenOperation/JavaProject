ba678fbd0128026b560d4a4c8be0311fda035c54
pauladamczyk <pauladamczyk@edv03.s180.local>
2017-04-11 16:55:05 +0200

    Überprüfung von eingebenen Daten erfolgreich getestet

--- JavaProjectj/src/BearbeitungsModule/UeberpruefungWohnung.java
+++ JavaProjectj/src/BearbeitungsModule/UeberpruefungWohnung.java
@@ -5,24 +5,38 @@ import java.util.Scanner;
 public class UeberpruefungWohnung {
 
 
-/*static 	
-static boolean Ueberpr�fungWonhungen(String Zeit, String Wohn []){
-for( int i W)
-}*/
-	
-static void Kontrolle(){
+
+static void UeberpruefungWonhungen(){
 String wohn[] =new String [2];
+wohn[0]= "12.3.2017";
+wohn[1]= "12.5.2017";
+boolean a=false;
+a=Kontrolle(wohn);
+Ausgabe(a);
+}
+
+public static boolean Kontrolle(String wohn[]){
+String test;
+boolean a=false;
 Scanner sc = new Scanner(System.in);		
-for(int i=0;i<wohn.length;i++){
 System.out.println( "Eingabe Datum");
-wohn[i]=sc.next();
+test=sc.next();
+
+
+for(int i=0;i<wohn.length;i++){
+if(wohn[i].equals(test)){
+	 a=true;
 }
 
-
-
-
-
 }
+return a;
+}
+public static void Ausgabe(boolean b){
+ if(b==false){
+	System.out.println("Wohnung sind Frei");
+	}else
+		System.out.println("Wonhungen sind zu dem angegeben Zeitpunkt belegt");
 	
+}	
 	
 }
