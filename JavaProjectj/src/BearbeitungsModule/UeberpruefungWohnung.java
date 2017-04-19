f17368f6b02541d617e2a64a0a1763988a368f3c
mfernitz <mfernitz@isdeblnnl065.iscout.local>
2017-04-19 11:45:03 +0200

    Kleine Method zur Datumsvalidierung als hilfestellung #MirwarLangweillig

--- JavaProjectj/src/BearbeitungsModule/UeberpruefungWohnung.java
+++ JavaProjectj/src/BearbeitungsModule/UeberpruefungWohnung.java
@@ -1,5 +1,8 @@
 package BearbeitungsModule;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class UeberpruefungWohnung {
@@ -14,8 +17,29 @@ wohn[1]= "12.5.2017-15.5.2017";
 String aber=wohn[0];
 
 boolean a=false;
-a=Kontrolle(wohn);
-Ausgabe(a);
+//0a=Kontrolle(wohn);
+//Ausgabe(a);
+datumsValidierung();
+}
+//Kleine Method zur Datumsvalidierung als hilfestellung #MirwarLangweillig
+public static void datumsValidierung(){
+	Scanner sc = new Scanner(System.in);
+	String d = sc.next();
+	SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy");
+	sdf.setLenient(false);
+	try {
+		Date date = sdf.parse(d);
+		System.out.println(sdf.parseObject(d));
+		if(date.after(new Date())){
+		
+			System.out.println("Ihre Datum eingabe war richtig");
+			date.after(new Date());
+		}
+		
+	} catch (ParseException e) {
+		System.out.println("Datum ist nicht richtig");
+	}
+	
 }
 
 public static boolean Kontrolle(String wohn[]){
