b2d567731f8407029ab7b7c66bc2aac592eee2b2
pauladamczyk <pauladamczyk@edv03.s180.local>
2017-04-19 16:41:25 +0200

    Mit der Klasse Date herumexperementiert

--- JavaProjectj/src/BearbeitungsModule/UeberpruefungWohnung.java
+++ JavaProjectj/src/BearbeitungsModule/UeberpruefungWohnung.java
@@ -1,6 +1,12 @@
 package BearbeitungsModule;
 
+
+//import java.text.DateFormat;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
 import java.util.Scanner;
+
+
 
 public class UeberpruefungWohnung {
 
@@ -8,8 +14,8 @@ public class UeberpruefungWohnung {
 
 static void UeberpruefungWonhungen(){
 String wohn[] =new String [2];
-wohn[0]= "12.3.2017-12.3.2017";
-wohn[1]= "12.5.2017-15.5.2017";
+wohn[0]= "12.03.2017-14.03.2017";
+wohn[1]= "12.05.2017-15.05.2017";
 
 String aber=wohn[0];
 
@@ -19,11 +25,21 @@ Ausgabe(a);
 }
 
 public static boolean Kontrolle(String wohn[]){
+Scanner sc = new Scanner(System.in);	
 String test;
+//String datum[]= wohn[0].split("-");
+//
+//test=datum[1];	
+// DateFormat df=new SimpleDateFormat("dd.MM.yyyy");
+
 boolean a=false;
-Scanner sc = new Scanner(System.in);		
+//
 System.out.println( "Eingabe Datum");
 test=sc.next();
+
+
+
+	
 
 
 for(int i=0;i<wohn.length;i++){
@@ -34,6 +50,7 @@ if(wohn[i].equals(test)){
 }
 return a;
 }
+
 public static void Ausgabe(boolean b){
  if(b==false){
 	System.out.println("Wohnung sind Frei");
