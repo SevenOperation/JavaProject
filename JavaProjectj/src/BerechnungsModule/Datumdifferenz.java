79645499bfac4fc4e42fa84d6ebdfa92c324580f
user <user@3206-S17>
2017-05-04 13:59:19 +0200

    Statistik methode

--- JavaProjectj/src/BerechnungsModule/Datumdifferenz.java
+++ JavaProjectj/src/BerechnungsModule/Datumdifferenz.java
@@ -1,5 +1,14 @@
 package BerechnungsModule;
 
+import EingabeModule.ArrayEinlesen;
+
 public class Datumdifferenz {
+	
+	public static String statistik(int wohn){
+		String [][][] wohnungen = ArrayEinlesen.readWohnungen();
+		for (int i = 0; i < wohnungen[wohn].length; i++){
+			
+		}
+	}
 
 }
