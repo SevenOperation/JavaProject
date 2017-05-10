6481c1b5330427e1bdc7c143b26edefadda6e73f
mfernitz <mfernitz@isdeblnnl065.iscout.local>
2017-05-10 10:50:38 +0200

    Some new Methods for Validation

--- JavaProjectj/src/BearbeitungsModule/Ueberpruefer.java
+++ JavaProjectj/src/BearbeitungsModule/Ueberpruefer.java
@@ -1,5 +1,10 @@
 package BearbeitungsModule;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Ueberpruefer {
 	public boolean registrierDatenPruefen(String [][] benutzer, String vorname, String nachname){
 		   if(benutzer != null){
@@ -42,6 +47,27 @@ public class Ueberpruefer {
 		if(wohnungen != null){
 			if(wohnungen[wohnung] != null && wohnungen[wohnung].length == 0){
 				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean stornierPruefer(String [][][] wohnungen, int wohnung, String vorname , String nachname, String zeitraum){
+		if(wohnungen != null &&  wohnungen[wohnung] != null && wohnungen[wohnung].length > 0){
+			for(int i = 0; i < wohnungen[wohnung].length; i++){
+				if(wohnungen[wohnung][i][0].equals(vorname) && wohnungen[wohnung][i][1].equals(nachname)){
+					String[] datum = zeitraum.split("-");
+					SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+					try {
+						Date anfang = sdf.parse(datum[0]);
+						Calendar c = Calendar.getInstance();
+						c.add(Calendar.DAY_OF_MONTH, 1);
+						anfang.after(c.getTime());
+					} catch (ParseException e) {
+						return false;
+					}
+					return true;
+				}
 			}
 		}
 		return false;
